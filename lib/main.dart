fe69497ad10ef2bb8d7dbf17b69df9cfa0644757
borba-pedro <127432041+borba-pedro@users.noreply.github.com>
2023-03-16 17:15:31 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto/model/dadospessoas.dart';
+import 'package:projeto/teladados.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'FYTNESS CAL',
       theme: ThemeData(
     
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TelaInicio()
     );
   }
 }
---
+++ lib/teladados.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TelaInicio extends StatelessWidget {
+  const TelaInicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
